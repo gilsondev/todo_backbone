d8a6fdd2ccb37e9368d0d822855551160828340a
gilsondev <contato@gilsondev.com>
2014-05-24 01:21:35 -0300

    Testing and insert status field

--- task/models.py
+++ task/models.py
@@ -5,3 +5,4 @@ from django.db import models
 
 class Task(models.Model):
     description = models.TextField()
+    status = models.CharField(max_length=30)
--- task/tests/test_models.py
+++ task/tests/test_models.py
@@ -8,7 +8,12 @@ from task.models import Task
 class TaskModelTest(TestCase):
     def setUp(self):
         self.fields = Task._meta.get_all_field_names()
-    def test_should_be_description_field(self):
+
+    def test_description_field(self):
         """Should contain description field"""
         self.assertIn("description", self.fields)
+
+    def test_status_field(self):
+        """Should contain status field"""
+        self.assertIn("status", self.fields)
 
