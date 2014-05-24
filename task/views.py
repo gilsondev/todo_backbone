87cec8325202ac8181307161817806b7655bb28a
gilsondev <contato@gilsondev.com>
2014-05-24 19:59:43 -0300

    Insert serializer class in view

--- task/views.py
+++ task/views.py
@@ -3,7 +3,9 @@
 from rest_framework import generics
 
 from task.models import Task
+from task.serializers import TaskSerializer
 
 
 class TaskListView(generics.ListAPIView):
     model = Task
+    serializer_class = TaskSerializer
