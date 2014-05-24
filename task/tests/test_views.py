7674dae6d1b3e513c0363b6b4e829fa9f789abcb
gilsondev <contato@gilsondev.com>
2014-05-24 20:16:03 -0300

    Refactor TaskTest

--- task/tests/test_views.py
+++ task/tests/test_views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.core.urlresolvers import reverse
+
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -9,5 +11,5 @@ class TasksTest(APITestCase):
         """
         Should list all tasks
         """
-        response = self.client.get('/api/todos/', format='json')
+        response = self.client.get(reverse('task_list'), format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
