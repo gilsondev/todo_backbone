d1b2805c2e3ef0170e170c4a6c1d39198313fe60
gilsondev <contato@gilsondev.com>
2014-05-24 21:17:25 -0300

    Remove route not used

--- task/urls.py
+++ task/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, url
 
-from task.views import (
-    TaskListView, TaskNewView,
-    TaskUpdateView, TaskCompleteView
-)
+from task.views import TaskListView, TaskNewView, TaskUpdateView
 
 urlpatterns = patterns('',
     url(r'^todo/$', TaskListView.as_view(), name='todo_list'),
     url(r'^todo/task/create/$', TaskNewView.as_view(), name='task_new'),
     url(r'^todo/task/(?P<pk>[\d+])/$', TaskUpdateView.as_view(),
         name='task_update'),
-    url(r'^todo/task/complete/(?P<pk>[\d+])/$', TaskCompleteView.as_view(),
-        name='task_complete'),
 )
 
